8f9f9ca95ea6a9b130e678dc63febb47e5c3d649
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2024-11-18 09:48:24 +0100

    add str files

---
+++ 00_str.py
@@ -0,0 +1,9 @@
+ # Szövegek (sztringek) összefűzhetőek
+print('Jöttem' + ' ' + 'láttam' + ' ' + 'győztem.')
+
+# Szövegek (sztringek) szorzásjellel megsokszorozhatóak
+print('ja' + 'j' * 7)
+
+# Más adattípusok szöveggé (sztringgé) alakíthatóak az str() függvénnyel
+number_str = (str(7.53))
+print(type(number_str))
